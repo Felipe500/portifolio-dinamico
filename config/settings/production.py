69eb04aac42d55da6b61bab7e57a9485b4054a0b
Jose Felipe <felipe.oliveira@blitzpay.com.br>
2023-12-12 16:32:37 -0300

    add logs for production server

--- config/settings/production.py
+++ config/settings/production.py
@@ -27,3 +27,34 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'statics'), os.path.join(BASE_DIR, 's
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/root/logs/my_portifolio/debug.log',
+        },
+        'file_info': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename':  '/root/logs/my_portifolio/debug_info.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+        'django.template': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+        },
+        'members_enable': {
+            'handlers': ['members_enable'],
+            'level': 'INFO',
+        },
+    },
+}
