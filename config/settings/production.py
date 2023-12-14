a2bef1ed3c5c86dc4e5cf607ef8cabe0b02eb963
Jose Felipe <felipe.oliveira@blitzpay.com.br>
2023-12-14 15:52:34 -0300

    config static files and media files

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,29 +4,13 @@ import os
 
 DEBUG = False
 
-# aws settings
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_DEFAULT_ACL = config('AWS_DEFAULT_ACL')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-# s3 static settings
-AWS_STATIC_LOCATION = 'static'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# s3 public media settings
-AWS_MEDIA_LOCATION = 'media'
-MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-)
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
 
 LOGGING = {
     'version': 1,
@@ -35,12 +19,12 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': '/root/logs/my_portifolio/debug.log',
+            'filename': '/root/logs/demos/portifolio-dev/debug.log',
         },
         'file_info': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename':  '/root/logs/my_portifolio/debug_info.log',
+            'filename':  '/root/logs/demos/portifolio-dev/debug_info.log',
         },
     },
     'loggers': {
