6595d784974525712823e69867daa3693c230c13
Jose Felipe <felipe.oliveira@blitzpay.com.br>
2023-12-19 11:46:22 -0300

    very set static

--- config/settings/production.py
+++ config/settings/production.py
@@ -23,38 +23,4 @@ DEFAULT_FILE_STORAGE = 'config.storage_backends.PublicMediaStorage'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'default': {
-            'format': '%(asctime)s %(levelname)s - %(name)s  %(message)s',
-        }
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'default',
-            'filename': '/root/logs/demos/portifolio-dev/debug.log',
-            'backupCount': 10,
-            'maxBytes': 5242880,  # 5*1024*1024 bytes (5MB)
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-        },
-        'django.request': {
-            'handlers': ['file'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'django.template': {
-            'handlers': ['file'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    },
-}
+
