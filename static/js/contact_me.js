d79f9a08ba99a0d075c2dce4cedd4948458a3ef5
Jose Felipe <felipe.oliveira@blitzpay.com.br>
2023-12-20 15:22:16 -0300

    refactor template and static/contact_me.js

--- static/js/contact_me.js
+++ static/js/contact_me.js
@@ -29,6 +29,7 @@ function post_form_contact_me(){
             if (data.message == 'success') {
                 console.log("atualizado com sucesso!");
                 $("#div_message").removeClass().addClass('alert alert-success alert-dismissible fade show')
+                $('#message_form').empty();
                 $('#message_form').append('Mensagem enviada com sucesso')
                 $("#div_message").css("display", "block").
                 css("background", "#fbf276c9").css("color", "#d24500");
@@ -39,6 +40,7 @@ function post_form_contact_me(){
             } else {
                 console.log("error ao cadastrar!");
                 $("#div_message").removeClass().addClass('alert alert-danger alert-dismissible fade show')
+                $('#message_form').empty();
                 $('#message_form').html('Verifique se existem campos não preenchidos!')
                 $("#div_message").css("display", "block").
                 css("background", "rgb(255, 216, 63)").css("color", "rgb(253, 0, 0)");
